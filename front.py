8952eafb59d9ff8b368e7a4030e503004b6a6037
KristinaValjarevic <kristina.valjarevic@gmail.com>
2021-06-24 17:54:01 +0200

    Created the class for the Add Project view in GUI.

--- front.py
+++ front.py
@@ -8,9 +8,12 @@ class InitialView(ttk.Frame):
         ttk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         self.parent.title("Jira Documentation Tracker")
-        ttk.Button(self, text="Add a New Project", command=self.add_project).grid(row=0, column=0, padx=20, pady=20)
-        ttk.Button(self, text="Update All Sheets", command=self.update_sheets).grid(row=0, column=1, pady=20)
-        ttk.Button(self, text="Review Jira Issues", command=self.review_issues).grid(row=0, column=2, padx=20, pady=20)
+        ttk.Button(self, text="Add a New Project", command=self.add_project)\
+            .grid(row=0, column=0, padx=20, pady=20)
+        ttk.Button(self, text="Update All Sheets", command=self.update_sheets)\
+            .grid(row=0, column=1, pady=20)
+        ttk.Button(self, text="Review Jira Issues", command=self.review_issues)\
+            .grid(row=0, column=2, padx=20, pady=20)
 
     def add_project(self):
         self.destroy()
@@ -29,21 +32,41 @@ class NewProject(ttk.Frame):
 
     def __init__(self, parent, *args, **kwargs):
         ttk.Frame.__init__(self, parent, *args, **kwargs)
-        parent.title("Add a New Project")
+        self.parent = parent
+        self.parent.title("Add a New Project")
+        ttk.Label(self, text="Enter the release number:")\
+            .grid(row=0, column=0, columnspan=5, padx=20, pady=10)
+        self.entry = ttk.Entry(self, width=20)
+        self.entry.grid(row=1, column=0, columnspan=5, padx=20, pady=10)
+        ttk.Label(self,
+                  text="Note: Make sure that all the filters for this project are set up in Jira"
+                       " and that they are correctly named.",
+                  wraplength=350, justify="center", foreground="grey")\
+            .grid(row=2, column=0, columnspan=5, padx=20, pady=10)
+        ttk.Button(self, text="Enter")\
+            .grid(row=3, column=1, padx=20, pady=10)
+        ttk.Button(self, text="Back", command=self.back)\
+            .grid(row=3, column=3, padx=20, pady=10)
+
+    def back(self):
+        self.destroy()
+        InitialView(self.parent).pack(side="top", fill="both", expand=True)
 
 
 class UpdateSheets(ttk.Frame):
 
     def __init__(self, parent, *args, **kwargs):
         ttk.Frame.__init__(self, parent, *args, **kwargs)
-        parent.title("Update All Sheets")
+        self.parent = parent
+        self.parent.title("Update All Sheets")
 
 
 class JiraIssues(ttk.Frame):
 
     def __init__(self, parent, *args, **kwargs):
         ttk.Frame.__init__(self, parent, *args, **kwargs)
-        parent.title("Review Jira Issues")
+        self.parent = parent
+        self.parent.title("Review Jira Issues")
 
 
 if __name__ == "__main__":
